08f0181e735e5f5f4bef727dff5aab69a1c73834
Andre Marques <95247935+andremarques27@users.noreply.github.com>
2023-08-16 16:18:23 -0300

    Add files via upload

---
+++ Unisinos-Lab1/exercicio4.py
@@ -0,0 +1,13 @@
+cep = (input("Digite o seu CEP: "))
+nomeRua = input("Digite o nome da sua rua: ")
+numeroCasa = input("Digite o número da sua casa: ")
+bairro = input("Digite o seu nome do seu bairro: ")
+cidade = input("Digite o nome da sua  cidade")
+estado = input("Digite o nome do seu Estado: ")
+
+print(" CEP:", cep, "\n",
+       "Rua:", nomeRua, "\n",
+       "Número:", numeroCasa, "\n",
+       "Bairro:", bairro, "\n",
+       "Cidade>", cidade, "\n",
+       "UF:", estado)
---
+++ Unisinos-Lab1/exercicioAvancado1.py
@@ -0,0 +1,7 @@
+a = float(input("Digite um número real: "))
+b = int(input("Digite um número inteiro: "))
+
+print(a, "x", b, "=", a * b, "\n",
+      a, "/", b, "=", a / b, "\n",
+      a, "+", b, "=", a + b, "e", a, "-", b, "=", a - b,"\n",
+      a, "^", b, "=", a ** b, "\n",)
