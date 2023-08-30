9d5c7c9ff10b5aac87f21164e48e8509b3a6d6d5
Andre Marques <95247935+andremarques27@users.noreply.github.com>
2023-08-30 00:44:38 -0300

    Delete exercicioExtra8.py

--- Unisinos-Lab1/exercicioExtra8.py
+++
@@ -1,6 +0,0 @@
-precoGasolina = float(input("Digite o valor da gasolina: "))
-dinheroDisponivel = float(input("Digite o valor disponivel: "))
-
-litrosComprado = dinheroDisponivel / precoGasolina  
-
-print(f"Foi colocado {litrosComprado:.2f} litros de gasolina no tanque.")
