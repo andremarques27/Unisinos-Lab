583a2795bffa4a63bdedf1e6af7f30e6b16778ec
Andre Marques <95247935+andremarques27@users.noreply.github.com>
2023-08-30 00:42:27 -0300

    Delete exercicio6.py

--- Unisinos-Lab1/exercicio6.py
+++
@@ -1,9 +0,0 @@
-a = int(input("Número 1: "))
-b = int(input("Número 2: "))
-c = int(input("Número 3: "))
-d = int(input("Número 4: "))
-e = int(input("Número 5: "))
-
-print(" A área do triangulo é:", (b * c) / 2, "\n"
-      "A soma do perimetro do retangulo é:", a + b + c + d, "\n"
-      "A área do circulo é:", 3.14 * (e ** 2))
