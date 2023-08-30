decfcba58bd0931b985eb77a32afc62cf3aeadbd
Andre Marques <95247935+andremarques27@users.noreply.github.com>
2023-08-30 00:44:12 -0300

    Delete exercicioExtra4.py

--- Unisinos-Lab1/exercicioExtra4.py
+++
@@ -1,6 +0,0 @@
-salarioFuncionario = float(input("Digite o salário do funcionário: "))
-
-salarioMinimo = 1320.0
-salarriosMinimos = salarioFuncionario / salarioMinimo
-
-print(f"O funcionário recebe um total de {salarriosMinimos:.2f} salarios minimos.")
