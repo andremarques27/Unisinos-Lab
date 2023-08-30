683b30e5c1f37e46b99f73b44ce98ada7a68f238
Andre Marques <95247935+andremarques27@users.noreply.github.com>
2023-08-30 00:42:52 -0300

    Delete exercicioAvancado1.py

--- Unisinos-Lab1/exercicioAvancado1.py
+++
@@ -1,7 +0,0 @@
-a = float(input("Digite um número real: "))
-b = int(input("Digite um número inteiro: "))
-
-print(a, "x", b, "=", a * b, "\n",
-      a, "/", b, "=", a / b, "\n",
-      a, "+", b, "=", a + b, "e", a, "-", b, "=", a - b,"\n",
-      a, "^", b, "=", a ** b, "\n",)
