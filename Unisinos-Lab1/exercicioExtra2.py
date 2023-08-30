d68f37e8d565f6b07ac1deb559bcf10c42e10923
Andre Marques <95247935+andremarques27@users.noreply.github.com>
2023-08-30 00:43:59 -0300

    Delete exercicioExtra2.py

--- Unisinos-Lab1/exercicioExtra2.py
+++
@@ -1,8 +0,0 @@
-salario = float(input("Digite seu salário atual: "))
-
-aumento = salario * 0.15
-salarioComAumento = salario + aumento
-imposto = salarioComAumento * 0.08
-salarioFinal = salarioComAumento - imposto
-
-input(f"Seu novo salário é: R$ {salarioFinal:.2f}")
