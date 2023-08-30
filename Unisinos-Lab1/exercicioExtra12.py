19f9c0ac5bc22300d771b04d85ac644986fda3d0
Andre Marques <95247935+andremarques27@users.noreply.github.com>
2023-08-30 00:43:34 -0300

    Delete exercicioExtra12.py

--- Unisinos-Lab1/exercicioExtra12.py
+++
@@ -1,7 +0,0 @@
-valorCompra = float(input("Digite o valor da compra: "))
-
-valorDesconto = valorCompra * 0.15
-valorFinal = valorCompra - valorDesconto
-
-print(f"O valo do desconto é: {valorDesconto} \n"
-      f"O valor final com desconto é: {valorFinal}")
